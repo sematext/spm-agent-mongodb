1f90dab3f1f7b991b8fb23053d7e0086e3e671a6
megastef <s.thies@bigdata-analyst.de>
2015-10-26 20:37:39 +0100

    add systemd service installer

---
+++ bin/spm-agent-mongodb-setup.sh
@@ -0,0 +1,33 @@
+# /usr/bin/env bash
+export SERVICE_NAME=spm-agent-mongodb.service
+export SERVICE_FILE="/etc/systemd/system/${SERVICE_NAME}"
+function generate_file() 
+{
+	echo -e "[Unit]\nDescription=SPM for MongoDB\nAfter=network.target\n[Service]\nRestart=always\nRestartSec=10 \nType=simple\n
+	ExecStart=$1 $2 $3\n[Install]\n
+	WantedBy=multi-user.target" > $SERVICE_FILE
+
+	echo "Service file $SERVICE_FILE:"
+	cat $SERVICE_FILE
+	systemctl enable $SERVICE_NAME
+	sleep 3 
+	systemctl status $SERVICE_NAME
+}
+
+token={$1-"$SPM_TOKEN"}
+mongodb={$2-"$SPM_MONGODB_URL"}
+
+if [[ -n "$token" && -n "$mongodb" ]] ; then 
+	command=$(which spm-mongodb)
+  generate_file $command $token $mongodb;
+else 
+	echo "Missing paramaters. Usage:"
+	echo "spm-agent-mongodb-setup.sh SPM_TOKEN MONGODB_URL (mongodb://dbuser:dbpassword@localhost:27017"
+	echo "Please obtain your application token from https://apps.sematext.com/"
+	read -p "SPM Token:" token
+	token=${token:-none}
+	read -p "MongoDB URL user:pass@host:port/db (localhost:27017/local):" mongodb
+	mongodb=${mongodb:-mongodb://localhost:27017/local}
+	command=$(which spm-mongodb)
+  generate_file $command $token $mongodb;
+fi 
