b66f8aebc827c14ee6c500becc0f34ed07208eff
megastef <s.thies@bigdata-analyst.de>
2015-11-02 19:09:04 +0100

    add upstart

--- bin/spm-agent-mongodb-setup.sh
+++ bin/spm-agent-mongodb-setup.sh
@@ -1,7 +1,25 @@
-# /usr/bin/env bash
-export SERVICE_NAME=spm-agent-mongodb.service
-export SERVICE_FILE="/etc/systemd/system/${SERVICE_NAME}"
-function generate_file() 
+#!/usr/bin/env bash
+
+SERVICE_NAME=spm-agent-mongodb
+SYSTEMD_SERVICE_FILE="/etc/systemd/system/${SERVICE_NAME}.service"
+UPSTART_SERVICE_FILE="/etc/systemd/system/${SERVICE_NAME}.service"
+
+function generate_upstart()
+{
+echo -e "description \"SPM for MongoDB\"
+start on runlevel [2345]
+stop on runlevel [06]
+respawn
+env SPM_MONGODB_URL=$3
+env SPM_TOKEN=$2
+chdir  /tmp
+exec $1 $2 $3" > /etc/init/${SERVICE_NAME}.conf
+CMD="service restart ${SERVICE_NAME}"
+echo $CMD
+sudo $CMD
+}
+
+function generate_systemd() 
 {
 echo -e \
 "
@@ -14,22 +32,35 @@ Restart=always\nRestartSec=10
 ExecStart=$1 $2 $3
 
 [Install]
-WantedBy=multi-user.target" > $SERVICE_FILE
+WantedBy=multi-user.target" > $SYSTEMD_SERVICE_FILE
 
 echo "Service file $SERVICE_FILE:"
-cat $SERVICE_FILE
+cat $SYSTEMD_SERVICE_FILE
 systemctl enable $SERVICE_NAME
 systemctl stop $SERVICE_NAME > /dev/null
 systemctl start $SERVICE_NAME
 
 }
 
+function install_script ()
+{
+	if [ -f /usr/share/upstart ]; then 
+		echo "Generate upstart script ${UPSTART_SERVICE_FILE}"
+		generate_upstart $1 $2 $3 	
+		return
+	fi
+	if [ -f /usr/lib/systemd ]; then 
+		echo "Generate systemd script "
+		generate_systemd $1 $2 $3 
+		return 
+	fi
+}
 
 if [[ -n "$1" && -n "$2" ]] ; then 
   token=$1
   mongodb=$2
 	command=$(which spm-mongodb)
-  generate_file $command $token $mongodb;
+  install_script $command $token $mongodb;
 else 
 	echo "Missing paramaters. Usage:"
 	echo " spm-agent-mongodb-setup.sh SPM_TOKEN MONGODB_URL (mongodb://dbuser:dbpassword@localhost:27017)"
@@ -39,5 +70,5 @@ else
 	read -p "MongoDB URL (mongodb://localhost:27017/local):" mongodb
 	mongodb=${mongodb:-mongodb://localhost:27017/local}
 	command=$(which spm-mongodb)
-  generate_file $command $token $mongodb;
+  install_script $command $token $mongodb;
 fi 
